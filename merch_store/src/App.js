f446d08ef7d825b02414b06420d04f1579e0fc8d
Ryan McMurtry <rkainoam@gmail.com>
2023-09-07 17:02:12 -0700

    configured BrowserRouter, fixed problem with nested routers

--- merch_store/src/App.js
+++ merch_store/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home";
 import ItemLayout from "./Components/ItemLayout";
--- merch_store/src/index.js
+++ merch_store/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
@@ -8,7 +7,7 @@ import { Provider } from 'react-redux'
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
