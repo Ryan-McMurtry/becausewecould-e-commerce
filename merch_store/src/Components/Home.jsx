a0ecad463eef3d6e3c058db14900cddc4848f087
Ryan McMurtry <rkainoam@gmail.com>
2023-10-15 19:39:53 -0700

    implementing a featured product section on the homepage

--- merch_store/src/Components/Home.jsx
+++ merch_store/src/Components/Home.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-import MerchList from "./MerchList"
+import Card from "./Card";
+import useFetch from "./Hooks/useFetch";
+import "../Styles/styles.css";
 
 function Home() {
+  const { data, loading, error } = useFetch("/items?populate=*");
+  const filterArr = new Array();
+
+  data.filter((item) => {
+    console.log(item?.attributes);
+    if (item?.attributes?.isNew === true) {
+      filterArr.push(item);
+    }
+  });
+
   return (
     <>
       <div className="container">
-        <h1>HOMEPAGE</h1>
+        <div className="featured">
+          <div className="featuredTitle">
+            <h2>Featured:</h2>
+          </div>
+          <div className="indivCards">
+            {loading
+              ? "Loading"
+              : filterArr?.map((item) => <Card item={item} key={item.id} />)}
+          </div>
+        </div>
       </div>
     </>
   );
