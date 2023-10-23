094cacf95a0a6e680830f44daecb01aa09dcbe10
Ryan McMurtry <rkainoam@gmail.com>
2023-10-23 00:47:01 -0700

    switched to react player for playback

--- merch_store/src/Components/ReactVideoPlayer.jsx
+++ merch_store/src/Components/ReactVideoPlayer.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import {DefaultPlayer as Video} from "react-html5video";
-import vid from "../Videos/8RO8-Townies.mp4";
+import ReactPlayer from "react-player";
 import "../Styles/styles.scss";
+import { Container } from "@mui/material";
 
 const ReactVideoPlayer = () => {
     return (
-        <Video className="homeVid">
-            <source src={vid} type="video/webm" />
-        </Video>
-    )
+      <div className="videoContainer">
+        <Container maxWidth="md" justify="center">
+          <div className="playerWrapper">
+            <ReactPlayer
+              className="player"
+              url="https://www.youtube.com/watch?v=EBcbiS7tfJw&ab_channel=8RO8"
+              width="80vw"
+              height="85vh"
+              playing={true}
+              muted={true}
+            />
+          </div>
+        </Container>
+      </div>
+    );
 }
 
 
